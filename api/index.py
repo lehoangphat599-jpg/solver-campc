bc3115e192e791f4009c71fe5f7039a71b6c49f9
lehoangphat599-jpg <lehoangphat599@gmail.com>
2026-08-04 22:27:59 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ fake_404 = """<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CamPC Real</title>
+    <title>Hoang Phat</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500;700&family=Plus+Jakarta+Sans:wght@600;700;800&display=swap" rel="stylesheet">
     <style>
